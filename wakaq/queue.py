7c8bc5943df94342b1c088b3dba6472977fa9688
Daniel Podrażka <podrazkafreelance@gmail.com>
2023-03-01 10:32:40 +0100

    Update queue.py

--- wakaq/queue.py
+++ wakaq/queue.py
@@ -6,16 +6,24 @@ from datetime import timedelta
 
 
 class Queue:
-    __slots__ = [
+    __slots__: Tuple[str, ...] = (
         "name",
         "priority",
         "prefix",
         "soft_timeout",
         "hard_timeout",
         "max_retries",
-    ]
+    )
 
-    def __init__(self, name=None, priority=-1, prefix=None, soft_timeout=None, hard_timeout=None, max_retries=None):
+    def __init__(
+        self,
+        name: str = None,
+        priority: int = -1,
+        prefix: str = None,
+        soft_timeout: Optional[Union[int, timedelta]] = None,
+        hard_timeout: Optional[Union[int, timedelta]] = None,
+        max_retries: Optional[int] = None,
+    ) -> None:
         self.prefix = re.sub(r"[^a-zA-Z0-9_.-]", "", prefix or "wakaq")
         self.name = re.sub(r"[^a-zA-Z0-9_.-]", "", name)
 
@@ -24,10 +32,22 @@ class Queue:
         except:
             raise Exception(f"Invalid queue priority: {priority}")
 
-        self.soft_timeout = soft_timeout.total_seconds() if isinstance(soft_timeout, timedelta) else soft_timeout
-        self.hard_timeout = hard_timeout.total_seconds() if isinstance(hard_timeout, timedelta) else hard_timeout
+        self.soft_timeout = (
+            soft_timeout.total_seconds()
+            if isinstance(soft_timeout, timedelta)
+            else soft_timeout
+        )
+        self.hard_timeout = (
+            hard_timeout.total_seconds()
+            if isinstance(hard_timeout, timedelta)
+            else hard_timeout
+        )
 
-        if self.soft_timeout and self.hard_timeout and self.hard_timeout <= self.soft_timeout:
+        if (
+            self.soft_timeout
+            and self.hard_timeout
+            and self.hard_timeout <= self.soft_timeout
+        ):
             raise Exception(
                 f"Queue hard timeout ({self.hard_timeout}) can not be less than or equal to soft timeout ({self.soft_timeout})."
             )
@@ -41,7 +61,9 @@ class Queue:
             self.max_retries = None
 
     @classmethod
-    def create(cls, obj, queues_by_name=None):
+    def create(
+        cls, obj: Union[Tuple[str, int], Tuple[int, str], str], queues_by_name: Dict[str, Any]
+    ) -> "Queue":
         if isinstance(obj, cls):
             if queues_by_name is not None and obj.name not in queues_by_name:
                 raise Exception(f"Unknown queue: {obj.name}")
@@ -61,9 +83,9 @@ class Queue:
             return cls(name=obj)
 
     @property
-    def broker_key(self):
+    def broker_key(self) -> str:
         return f"{self.prefix}:{self.name}"
 
     @property
-    def broker_eta_key(self):
+    def broker_eta_key(self) -> str:
         return f"{self.prefix}:eta:{self.name}"
